b314df93b4e0f52943d72c7603d7559fd8073eb5
Lukas Alexandre <lukas@codelogic.me>
2013-12-12 05:02:24 -0500

    Setting up packaging task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,49 @@
 module.exports = function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '<%= pkg.banner.divider %>' +
+              '<%= pkg.banner.project %>' +
+              '<%= pkg.banner.copyright %>' +
+              '<%= pkg.banner.license %>' +
+              '<%= pkg.banner.licenseLink %>' +
+              '<%= pkg.banner.divider %>' +
+              '\n' +
+              '// Version: <%= pkg.version %> | From: <%= grunt.template.today("dd-mm-yyyy") %>\n\n'
+    },
     jasmine: {
       src: 'src/*.js',
       options: {
         specs: 'specs/*_spec.js'
       }
     },
+    concat: {
+      options: {
+        separator: '\n',
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+        src: ['src/**/*.js'],
+        dest: '<%= pkg.name %>'
+      }
+    },
     uglify: {
       options: {
-        // the banner is inserted at the top of the output
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        banner: '<%= meta.banner %>'
       },
       stik: {
         files: {
-         'stik.min.js': ['src/stik.js']
+         'stik.min.js': ['stik.js']
         }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('test', 'jasmine');
-
-  grunt.registerTask('pack', ['uglify']);
+  grunt.registerTask('pack', ['concat', 'uglify']);
 };
