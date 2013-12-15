aec1fc72e66c4ff5bb8f7673cc2f98afa7b4189c
Lukas Alexandre <lukas@codelogic.me>
2013-12-15 12:00:29 -0500

    Fixing some warnings from CodeClimate

--- src/manager.js
+++ src/manager.js
@@ -5,7 +5,7 @@ window.stik || (window.stik = {});
     this.$$contexts = [];
     this.$$executionUnits = {};
     this.$$modules = modules;
-  };
+  }
 
   Manager.prototype.$register = function(controller, action, executionUnit){
     if (!executionUnit)
@@ -18,14 +18,15 @@ window.stik || (window.stik = {});
   Manager.prototype.$storeExecutionUnit = function(controller, action, executionUnit){
     this.$$executionUnits[controller] || (this.$$executionUnits[controller] = {});
 
-    if (this.$$executionUnits[controller][action])
+    if (this.$$executionUnits[controller][action]){
       throw "Controller and Action already exist!";
+    }
 
     this.$$executionUnits[controller][action] = executionUnit;
   };
 
   Manager.prototype.$storeContext = function(controller, action, template, executionUnit){
-    newContext = this.$createContext(controller, action, template, executionUnit);
+    var newContext = this.$createContext(controller, action, template, executionUnit);
     this.$$contexts.push(newContext);
     return newContext;
   };
@@ -36,8 +37,7 @@ window.stik || (window.stik = {});
 
   Manager.prototype.$findTemplate = function(controller, action, DOMInjection){
     var DOMHandler = document;
-    if (DOMInjection)
-      DOMHandler = DOMInjection;
+    if (DOMInjection) { DOMHandler = DOMInjection; }
 
     var selector = "[data-controller=" + controller + "][data-action=" + action + "]";
     return DOMHandler.querySelectorAll(selector);
@@ -47,19 +47,20 @@ window.stik || (window.stik = {});
     var controller, action, executionUnit;
     var boundAny;
 
-    if (Object.keys(this.$$executionUnits).length === 0)
+    if (Object.keys(this.$$executionUnits).length === 0){
       throw "no execution units available";
+    }
 
     for (controller in this.$$executionUnits) {
       for (action in this.$$executionUnits[controller]) {
         executionUnit = this.$$executionUnits[controller][action];
-        if (this.$bindExecutionUnit(controller, action, executionUnit))
+        if (this.$bindExecutionUnit(controller, action, executionUnit)){
           boundAny = true;
-      };
-    };
+        }
+      }
+    }
 
-    if (!boundAny)
-      throw "no templates were bound";
+    if (!boundAny) { throw "no templates were bound"; }
   };
 
   Manager.prototype.$bindExecutionUnit = function(controller, action, executionUnit){
@@ -71,7 +72,7 @@ window.stik || (window.stik = {});
       this.$markAsBound(templates[i]);
       context = this.$storeContext(controller, action, templates[i], executionUnit);
       context.$load(this.$$modules);
-    };
+    }
 
     return templates.length > 0;
   };
