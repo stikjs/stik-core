f8a172518a61106a7ad27408468964226aa4edbc
Lukas Alexandre <lukas@codelogic.me>
2014-04-19 21:34:23 +0200

    Extracting $data from core

--- stik.js
+++ stik.js
@@ -639,34 +639,3 @@ window.stik.boundary({
     return obj;
   }
 });
-
-window.stik.boundary({
-  as: "$data",
-  resolvable: true,
-  to: function( $template ){
-    var attrs = {},
-        attr, name;
-
-    for ( attr in $template.attributes ) {
-      if ( $template.attributes[ attr ].value ) {
-        name = $template.attributes[ attr ].name;
-        if (name.match(/^data-/m)) {
-          attrs[ parseName( name ) ] =
-            $template.attributes[ attr ].value;
-        }
-      }
-    }
-
-    function parseName( name ){
-      return toCamelCase( name.match( /(data-)(.+)/ )[ 2 ] );
-    }
-
-    function toCamelCase(name){
-      return name.replace( /-([a-z])/g, function( match ){
-        return match[ 1 ].toUpperCase();
-      });
-    }
-
-    return attrs;
-  }
-});
